d22d0139612f5430cedaf65fe0f5b245fb07b4cb
MishirJain26 <mishirjain1@gmail.com>
2026-07-14 23:37:00 +0530

    feat(module2): implement UserSkill model with custom enums and encapsulation

---
+++ src/main/java/org/skillbridge/model/UserSkill.java
@@ -0,0 +1,79 @@
+package org.skillbridge.model;
+
+public class UserSkill
+{
+    // Defining the custom Enums
+    public enum SkillType {
+        TEACHING,
+        LEARNING
+    }
+
+    public enum SkillLevel {
+        BEGINNER,
+        INTERMEDIATE,
+        ADVANCED
+    }
+    private int userSkillId;
+    private int skillId;
+    private int userId;
+    private SkillType skillType;
+    private SkillLevel skillLevel;
+    private boolean isActive;
+
+    public UserSkill(int userSkillId, int skillId, int userId, SkillType skillType, SkillLevel skillLevel, boolean isActive) {
+        this.userSkillId = userSkillId;
+        this.skillId = skillId;
+        this.userId = userId;
+        this.skillType = skillType;
+        this.skillLevel = skillLevel;
+        this.isActive = isActive;
+    }
+
+    public int getUserSkillId() {
+        return userSkillId;
+    }
+
+    public void setUserSkillId(int userSkillId) {
+        this.userSkillId = userSkillId;
+    }
+
+    public int getSkillId() {
+        return skillId;
+    }
+
+    public void setSkillId(int skillId) {
+        this.skillId = skillId;
+    }
+
+    public int getUserId() {
+        return userId;
+    }
+
+    public void setUserId(int userId) {
+        this.userId = userId;
+    }
+
+    public SkillType getSkillType() {
+        return skillType;
+    }
+
+    public void setSkillType(SkillType skillType) {
+        this.skillType = skillType;
+    }
+
+    public SkillLevel getSkillLevel() {
+        return skillLevel;
+    }
+
+    public void setSkillLevel(SkillLevel skillLevel) {
+        this.skillLevel = skillLevel;
+    }
+
+    public boolean isActive() {
+        return isActive;
+    }
+
+    public void setActive(boolean active) {
+        isActive = active;
+    }
+}
