b36d86f56491cece402b314ca31cdb465ae24cb8
mrparv00001 <mrparv00001@gmail.com>
2026-07-15 00:10:42 +0530

    Initial SkillBridge project setup

---
+++ org/example/TestConnection.java
@@ -0,0 +1,12 @@
+package org.example;
+
+import org.example.database.DBConnection;
+
+public class TestConnection {
+
+    public static void main(String[] args) {
+
+        DBConnection.getConnection();
+
+    }
+}
---
+++ org/example/database/DBConnection.java
@@ -0,0 +1,31 @@
+package org.example.database;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class DBConnection {
+
+    private static final String URL =
+            "jdbc:mysql://gateway01.ap-southeast-1.prod.aws.tidbcloud.com:4000/skillbridge?sslMode=VERIFY_IDENTITY&enabledTLSProtocols=TLSv1.2,TLSv1.3";
+
+    private static final String USER =
+            "24cPrP7jfNiAnuf.root";
+
+    private static final String PASSWORD =
+            "S1EnSarxsg8oMhzT";
+
+    public static Connection getConnection() {
+
+        try {
+            Connection con = DriverManager.getConnection(URL, USER, PASSWORD);
+            System.out.println("✅ Connected Successfully!");
+            return con;
+
+        } catch (SQLException e) {
+            System.out.println("❌ Connection Failed");
+            e.printStackTrace();
+            return null;
+        }
+    }
+}
